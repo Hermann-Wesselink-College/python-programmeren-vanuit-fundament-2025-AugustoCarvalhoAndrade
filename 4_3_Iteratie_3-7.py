c8bc7bc32c1ef09b4880eaf09a742ff235688fa2
AugustoCarvalhoAndrade <augustocarvalhoandrade@gmail.com>
2025-10-13 08:26:44 +0000

    modified:   4_3_Iteratie_3-7.py

--- 4_3_Iteratie_3-7.py
+++ 4_3_Iteratie_3-7.py
@@ -7,6 +7,33 @@ else:
     print(str(i) + " keer " + str(getal) + " = " + str(i * getal))
 
 # opdracht 4
-
+mile = 1.609
+print("Km\tMiles")
+for i in range(1,11):
+    miles = i * mile
+print(str(i) + "\t" + str(miles))
 
 # opdracht 5
+getal = int(input("Van welk getal wil je de faculteit?"))
+uitkomst = 1
+for i in range(1, getal + 1):
+  uitkomst = uitkomst * i
+ 
+print(str(uitkomst))
+
+# opdracht 6
+for i in range(0,50,2):
+  print(i)
+
+# opdracht 7
+minPlus = False
+uitkomst = 1
+ 
+for i in range(3,100000,2):
+  if minPlus:
+    uitkomst = uitkomst + (1/i)
+  else:
+    uitkomst = uitkomst - (1/i)
+  minPlus = not minPlus
+ 
+print(4*uitkomst)
